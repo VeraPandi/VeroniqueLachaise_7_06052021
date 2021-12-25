587b0d3c7e3dfcb50b6e329a449c456f1420603c
VeraPandi <84042427+VeraPandi@users.noreply.github.com>
2021-12-25 04:48:59 +0100

    Add : CSS styles on the index page.

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
 import { recipes } from "./api/recipes.js";
-
-async function getData(recipes) {
-   let response = await fetch("api/recipes.js");
-   console.log(recipes);
-   return recipes;
-}
-
-getData(recipes);
